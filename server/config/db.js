da26f7d925d3c2391cb0a780ccf43a79948de48a
Aoichai sorayan <apichailove2544@gmail.com>
2024-05-14 12:14:44 +0700

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/db_chui')
+        console.log('Connected DB success')
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+
+module.exports = connectDB
